036adf7cc279ba88e309be8d6216a28f5048de9f
karankharole <karankharole2000@gmail.com>
2022-12-05 21:56:57 +0530

    refactor all 3 to one generic method and find the maximum

--- MaxNumberCheck.cs
+++ MaxNumberCheck.cs
@@ -6,12 +6,48 @@ using System.Threading.Tasks;
 
 namespace Day13_Generics
 {
-   
-    
-        public class MaxNumberCheck
-        {
+
+
+    public class MaxNumberCheck
+    {
         // integer type
-       
+        public static int MaxIntegerNumber(int val1, int val2, int val3)
+        {
+            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) >= 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) > 0 && val1.CompareTo(val3) >= 0)
+            {
+                return val1;
+            }
+            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0 || val2.CompareTo(val1) >= 0 && val2.CompareTo(val3) > 0 || val2.CompareTo(val1) > 0 && val2.CompareTo(val3) >= 0)
+            {
+                return val2;
+            }
+
+            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val1) > 0 || val3.CompareTo(val1) >= 0 && val3.CompareTo(val1) > 0 || val3.CompareTo(val1) > 0 && val3.CompareTo(val1) >= 0)
+            {
+                return val3;
+            }
+            return val1;
+        }
+
+        // double type
+        public static double MaxFloatNumber(double val1, double val2, double val3)
+        {
+            if (val1.CompareTo(val2) > 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) >= 0 && val1.CompareTo(val3) > 0 || val1.CompareTo(val2) > 0 && val1.CompareTo(val3) >= 0)
+            {
+                return val1;
+            }
+            if (val2.CompareTo(val1) > 0 && val2.CompareTo(val3) > 0 || val2.CompareTo(val1) >= 0 && val2.CompareTo(val3) > 0 || val2.CompareTo(val1) > 0 && val2.CompareTo(val3) >= 0)
+            {
+                return val2;
+            }
+
+            if (val3.CompareTo(val1) > 0 && val3.CompareTo(val1) > 0 || val3.CompareTo(val1) >= 0 && val3.CompareTo(val1) > 0 || val3.CompareTo(val1) > 0 && val3.CompareTo(val1) >= 0)
+            {
+                return val3;
+            }
+            return val1;
+        }
+
         // char type
         public static string MaxStringNumber(string val1, string val2, string val3)
         {
@@ -31,5 +67,4 @@ namespace Day13_Generics
             return val1;
         }
     }
-    
 }
--- Program.cs
+++ Program.cs
@@ -8,8 +8,15 @@ namespace Day13_Generics
         {
 
 
+            int intArray = MaxNumberCheck.MaxIntegerNumber(23, 42, 12);
+            Console.WriteLine("\nMax integer number is: " + intArray);
+
+            double MaxFloat = MaxNumberCheck.MaxFloatNumber(33.2, 23.4, 12.5);
+            Console.WriteLine("\nMax float number is: " + MaxFloat);
+
             string strArr = MaxNumberCheck.MaxStringNumber("karan", "chetan", "yogesh");
             Console.WriteLine("\nMax string is: " + strArr);
+
         }
     }
 }
