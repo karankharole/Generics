6050bb667d764c21830630180b4ebd240d9d036a
karankharole <karankharole2000@gmail.com>
2022-12-05 19:41:58 +0530

    printing array generics class

--- Program.cs
+++ Program.cs
@@ -1,12 +1,20 @@
-﻿namespace Day13_Generics
+﻿
+
+namespace Day13_Generics
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
-            int output = MaxNumberCheck.MaxIntegerNumber(8347, 8778, 884);
-            Console.WriteLine("\nMax integer number is: " + output);
+            int[] intArray = { 1, 2, 3, 4, 5 };
+            double[] doubleArray = { 1.1, 2.2, 3.3, 4.4 };
+            char[] charArray = { 'H','E', 'L', 'L', 'O' };
+
+            new PrintArray<int>(intArray).toPrint();
+            new PrintArray<double>(doubleArray).toPrint();
+            new PrintArray<char>(charArray).toPrint();
+
         }
     }
 }
